e3a10131c14bf73b672a8cd424cdad021adb40b1
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-27 21:19:58 +0200

    Implement TestCase

--- TestProjects/ReverseWords.Test/ReverseWordsHandlerTests.cs
+++ TestProjects/ReverseWords.Test/ReverseWordsHandlerTests.cs
@@ -8,5 +8,26 @@ namespace ReverseWords.Test
     [TestFixture]
     class ReverseWordsHandlerTests
     {
+        private ReverseWordsHandler _reverseWordsHandler;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _reverseWordsHandler = new ReverseWordsHandler();
+        }
+
+
+        [Test]
+        public void GetReverseWord_WhenPassSentence_ShouldReturnTrue()
+        {
+            var inputSentence = "Das sind nicht die Droiden, die Ihr sucht";
+            var outputSentence = "sucht Ihr die Droiden, die nicht sind Das";
+
+            var reverse = _reverseWordsHandler.GetReverseWord(inputSentence);
+
+            Assert.That(reverse, Is.EqualTo(outputSentence));
+        }
+
     }
 }
