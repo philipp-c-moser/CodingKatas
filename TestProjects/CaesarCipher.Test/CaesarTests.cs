b56c252af1489de68184c42b22990d2dbb5aa17d
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-27 23:02:07 +0200

    Implement TestCases for Encryption and Decryption

---
+++ TestProjects/CaesarCipher.Test/CaesarTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CaesarCipher.Test
+{
+    [TestFixture]
+    class CaesarTests
+    {
+
+        private Caesar _caesar;
+        private string _plainText;
+        private string _encryptedText;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _caesar = new Caesar(8);
+
+            _plainText = "Das sind nicht die Droiden, die Ihr sucht!";
+            _encryptedText = "LiAhAqvlhvqkpBhlqmhLzwqlmv(hlqmhQpzhACkpBf";
+        }
+
+
+        [Test]
+        public void GetEncryptedString_WhenPassedPlainString_ReturnEncrypted()
+        {
+            var encryption = _caesar.GetEncryptedString(_plainText);
+
+            Assert.That(encryption, Is.EqualTo(_encryptedText));
+        }
+
+
+        [Test]
+        public void GetDecryptedString_WhenPassedEncryptedString_ReturnPlain()
+        {
+            var decryption = _caesar.GetDecryptedString(_encryptedText);
+
+            Assert.That(decryption, Is.EqualTo(_plainText));
+        }
+
+    }
+}
