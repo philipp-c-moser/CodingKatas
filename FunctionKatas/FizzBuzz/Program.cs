2ea422e565648563aedeb1d67e7c530c6ee85891
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-27 20:52:41 +0200

    Restructured Folders and moved FizzBuzz to FunctionKatas Folder

---
+++ FunctionKatas/FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzHandler fizzBuzzHandler = new FizzBuzzHandler();
+
+            Console.WriteLine("FizzBuzz - FunctionKata");
+
+            Console.WriteLine("Please enter the amount of numbers, you want to generate:");
+
+            var amount = Convert.ToInt32(Console.ReadLine());
+
+
+
+            var fizzBuzzString = fizzBuzzHandler.StartFizzBuzz(amount);
+
+            foreach (var fizzBuzz in fizzBuzzString)
+            {
+                Console.WriteLine(fizzBuzz);
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
