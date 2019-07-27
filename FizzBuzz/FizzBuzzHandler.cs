7f5879d485e768258a361e87645ed8eafe556c07
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-27 18:54:49 +0200

    Add method to ConvertNumberToFizzBuzz

--- FizzBuzz/FizzBuzzHandler.cs
+++ FizzBuzz/FizzBuzzHandler.cs
@@ -7,12 +7,23 @@ namespace FizzBuzz
     class FizzBuzzHandler
     {
 
+        public string ConvertNumberToFizzBuzz(int number)
+        {
+            if (IsDivisibleByThree(number) && IsDivisibleByFive(number)) return "FizzBuzz";
+            if (IsDivisibleByThree(number)) return "Fizz";
+            if (IsDivisibleByFive(number)) return "Buzz";
+
+            return Convert.ToString(number);
+        }
+
+
+
         private bool IsDivisibleByThree(int number)
         {
             return number % 3 == 0;
         }
 
-        private bool IsDivisibleByFivev(int number)
+        private bool IsDivisibleByFive(int number)
         {
             return number % 5 == 0;
         }
