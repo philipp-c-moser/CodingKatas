11464a02b19b6c7f79eb8065fd33f67da90ee6bc
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-27 21:06:43 +0200

    Functionality for Reversing Words

--- FunctionKatas/ReverseWords/Program.cs
+++ FunctionKatas/ReverseWords/Program.cs
@@ -6,7 +6,13 @@ namespace ReverseWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Reverse Words - Functional Kata");
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ FunctionKatas/ReverseWords/ReverseWordsHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseWords
+{
+    public class ReverseWordsHandler
+    {
+
+        public string GetReverseWord(string word)
+        {
+            return string.Join(" ", word.Split(' ').Reverse());
+        }
+
+    }
+}
