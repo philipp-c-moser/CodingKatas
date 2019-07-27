b7b7f8a57dc94ec2b50e1723c6a1b0d62686a09a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-27 19:06:39 +0200

    Add TestFixture and use NUnit.Framework

--- TestProjects/FizzBuzz.Test/FizzBuzzHandlerTests.cs
+++ TestProjects/FizzBuzz.Test/FizzBuzzHandlerTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace FizzBuzz.Test
 {
+    [TestFixture]
     class FizzBuzzHandlerTests
     {
     }
