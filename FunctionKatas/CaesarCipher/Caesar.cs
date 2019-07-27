2f3d72c6ff2c73ad49896ab87c6e6208e001df40
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-27 21:27:29 +0200

    Create Caesar.cs

---
+++ FunctionKatas/CaesarCipher/Caesar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCipher
+{
+    public class Caesar
+    {
+
+
+
+        public string Encrypt(string plain)
+        {
+
+        }
+
+
+
+
+    }
+}
