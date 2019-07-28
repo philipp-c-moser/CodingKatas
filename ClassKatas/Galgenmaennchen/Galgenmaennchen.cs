302c18afe149c825b5dae7b02139d4e3a30b8ea0
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-28 12:00:10 +0200

    Import Galgenmaennchen Project from other Solution

---
+++ ClassKatas/Galgenmaennchen/Galgenmaennchen.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galgenmaennchen
+{
+    public class Galgenmaennchen
+    {
+
+        private string _searchedWord;
+        private int _rounds;
+        private List<string> _guessWord;
+
+        public Galgenmaennchen(string searchedWord)
+        {
+            this._searchedWord = searchedWord;
+            _guessWord = new List<string>();
+            _rounds = 0;
+
+            for (int i = 0; i < _searchedWord.Length; i++)
+            {
+                _guessWord.Add("-");
+            }
+        }
+
+        public void StartGuessing()
+        {
+            while (IsSolved() == false)
+            {
+                Console.WriteLine("Bitte gib einen Buchstaben ein:");
+
+                var input = "";
+
+                var tempInput = Console.ReadLine();
+
+                if (tempInput != "")
+                {
+                    input = tempInput.Substring(0, 1);
+                }
+
+
+                Console.WriteLine(GuessLetter(input));
+            }
+        }
+
+        public int GetNumberOfRounds()
+        {
+            return this._rounds;
+        }
+
+
+        private string GuessLetter(string letter)
+        {
+            _rounds++;
+
+            foreach (var position in GetAllPositionsOfLetter(letter))
+            {
+                _guessWord[position] = letter;
+            }
+
+            return GenerateStringFromGuessedWord();
+        }
+
+        private bool IsSolved()
+        {
+            // TODO: If GuessWord does have a "-" to guess, the Galgenmaennchen will never be solved!
+            if (_guessWord.IndexOf("-") == -1)
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+
+        private List<int> GetAllPositionsOfLetter(string letter)
+        {
+            List<int> positions = new List<int>();
+
+
+            for (int i = 0; i < _searchedWord.Length; i++)
+            {
+                if (_searchedWord.Substring(i, 1) == letter)
+                {
+                    positions.Add(i);
+                }
+            }
+
+            return positions;
+        }
+
+
+        private string GenerateStringFromGuessedWord()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var letter in _guessWord)
+            {
+                sb.Append(letter);
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ ClassKatas/Galgenmaennchen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Galgenmaennchen
+{
+    class Program
+    {
+        private static Galgenmaennchen _galgenmaennchen;
+
+
+
+        static void Main(string[] args)
+        {
+            _galgenmaennchen = new Galgenmaennchen("Entwurfsmuster");
+
+            _galgenmaennchen.StartGuessing();
+
+            Console.WriteLine("Anzahl Runden: " + _galgenmaennchen.GetNumberOfRounds());
+
+            Console.ReadKey();
+        }
+    }
+}
