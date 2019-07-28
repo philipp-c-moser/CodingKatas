41ea79dc83dcb74a4aedbe9341487b060cd152c0
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-28 13:15:49 +0200

    Added New Project "Tannenbaum" for Function Katas

---
+++ FunctionKatas/Tannenbaum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tannenbaum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tannenbaum - Function Kata");
+        }
+    }
+}
