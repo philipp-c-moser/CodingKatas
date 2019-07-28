7fd35c1e1abc1375dc6aef01ddec0071879cb8ba
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-28 22:25:28 +0200

    Implemented check for values that are not matching to maxLength Rest

--- FunctionKatas/WordWrap/WordWrap.cs
+++ FunctionKatas/WordWrap/WordWrap.cs
@@ -13,16 +13,29 @@ namespace WordWrap
             var length = text.Length;
             var outputString = new StringBuilder();
 
-            for (var i = 0; i < length; i = i + maxLength)
+            for (var i = 0; i <= length; i = i + maxLength)
             {
-                var item = text.Substring(i, maxLength);
+                var item = "";
+                if (IsRestInTextAvailable(length, i, maxLength))
+                {
+                    item = text.Substring(i, maxLength);
+                }
+                else
+                {
+                    item = text.Substring(i, length - i);
+                }
 
                 outputString.Append(item);
                 outputString.Append("\n");
             }
 
             return outputString.ToString();
+        }
 
+
+        private bool IsRestInTextAvailable(int length, int startValue, int maxLength)
+        {
+            return length - (startValue + maxLength) >= 0;
         }
 
 
