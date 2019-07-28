7cd5f1120fb5bd11865f4ef05a49c9a7c5cb93c5
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-28 22:25:43 +0200

    Impelemented UserInterface for inserting values

--- FunctionKatas/WordWrap/Program.cs
+++ FunctionKatas/WordWrap/Program.cs
@@ -6,8 +6,18 @@ namespace WordWrap
     {
         static void Main(string[] args)
         {
+            WordWrap wordWrap = new WordWrap();
+
             Console.WriteLine("Word Wrap - Function Kata");
 
+            Console.WriteLine("Please insert a Sentence, you want to wrap:");
+            var textInput = Console.ReadLine();
+
+
+            Console.WriteLine("Thank you. Please specify the amount of letters before wrapping:");
+            var maxLengthInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(wordWrap.Wrap(textInput, maxLengthInput));
 
             Console.ReadKey();
         }
