ebd3cde323e6716ed089ffbcdceac6a96c6bacb1
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-28 22:03:43 +0200

    Add new Project "WordWrap"

---
+++ FunctionKatas/WordWrap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WordWrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Word Wrap - Function Kata");
+
+
+            Console.ReadKey();
+        }
+    }
+}
