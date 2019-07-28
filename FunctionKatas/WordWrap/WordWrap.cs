c2688815d9091add832935237ab38ed6ee80c797
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-28 22:13:15 +0200

    Implement Logic for breaking the words based on a given maxLength

--- FunctionKatas/WordWrap/WordWrap.cs
+++ FunctionKatas/WordWrap/WordWrap.cs
@@ -6,5 +6,25 @@ namespace WordWrap
 {
     class WordWrap
     {
+
+
+        public string Wrap(string text, int maxLength)
+        {
+            var length = text.Length;
+            var outputString = new StringBuilder();
+
+            for (var i = 0; i < length; i = i + maxLength)
+            {
+                var item = text.Substring(i, maxLength);
+
+                outputString.Append(item);
+                outputString.Append("\n");
+            }
+
+            return outputString.ToString();
+
+        }
+
+
     }
 }
