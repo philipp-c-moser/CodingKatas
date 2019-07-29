79b16196e0fa27346bc2d9aec6b2b50c59b5dc58
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-29 20:06:53 +0200

    RBM: Add UserInput Values

--- FunctionKatas/RussischeBauernmultiplikation/Program.cs
+++ FunctionKatas/RussischeBauernmultiplikation/Program.cs
@@ -6,7 +6,18 @@ namespace RussischeBauernmultiplikation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Russische Bauernmultiplikation - Function Kata");
+
+            Console.WriteLine("Bitte geben Sie Faktor 1 an:");
+            var factor01 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Bitte geben Sie Faktor 2 an:");
+            var factor02 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.ReadKey();
         }
     }
 }
