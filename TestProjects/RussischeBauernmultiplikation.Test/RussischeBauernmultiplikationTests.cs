25e3e1f6a1b7da80523f200759ef26edca3bb9b5
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-29 20:36:17 +0200

    RBM: Implement two TestCases

--- TestProjects/RussischeBauernmultiplikation.Test/RussischeBauernmultiplikationTests.cs
+++ TestProjects/RussischeBauernmultiplikation.Test/RussischeBauernmultiplikationTests.cs
@@ -17,6 +17,23 @@ namespace RussischeBauernmultiplikation.Test
             _russischeBauernmultiplikation = new RussischeBauernmultiplikation();
         }
 
+        [Test]
+        public void Multiplicate_WithTwoFactors_ReturnCorrectFactor()
+        {
+            var result = _russischeBauernmultiplikation.Multiplicate(968, 93277);
+
+            Assert.That(result, Is.EqualTo(90292136));
+        }
+
+
+        [Test]
+        public void Multiplicate_WithTwoFactorsReverse_ReturnCorrectFactor()
+        {
+            var result = _russischeBauernmultiplikation.Multiplicate(93277, 968);
+
+            Assert.That(result, Is.EqualTo(90292136));
+        }
+
 
 
 
