adb3ac54f58b50fb3f4859528e7859499b64412b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-29 20:15:49 +0200

    RBM: Setup of MultiplicateMethod

--- FunctionKatas/RussischeBauernmultiplikation/RussischeBauernmultiplikation.cs
+++ FunctionKatas/RussischeBauernmultiplikation/RussischeBauernmultiplikation.cs
@@ -9,9 +9,14 @@ namespace RussischeBauernmultiplikation
 
         public int Multiplicate(int factor1, int factor2)
         {
+            if (factor1 == null || factor2 == null)
+            {
+                throw new ArgumentNullException();
+            }
 
+            var valuesToSumUp = 0;
 
-
+         
 
             return 1;
 
