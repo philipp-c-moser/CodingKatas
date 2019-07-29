e459642950f124a5ce6b52830d22d5ac3acd6fb2
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-29 18:54:15 +0200

    FizzBuzz.Test: Add new TestClass for NumberGenerator

---
+++ TestProjects/FizzBuzz.Test/NumberGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace FizzBuzz.Test
+{
+    [TestFixture]
+    class NumberGeneratorTests
+    {
+
+
+
+
+
+    }
+}
