2659372a5194d698a0c13fb5cb35c27207e6361c
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-30 19:56:34 +0200

    CTD: Create new Project

---
+++ FunctionKatas/ConfigToDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigToDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
