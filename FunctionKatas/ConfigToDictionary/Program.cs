d0f2cbf32cb02ae83289cae30ca669849c068bc4
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-30 20:17:03 +0200

    CTD: Set Up Program.cs with User Input

--- FunctionKatas/ConfigToDictionary/Program.cs
+++ FunctionKatas/ConfigToDictionary/Program.cs
@@ -6,13 +6,15 @@ namespace ConfigToDictionary
     {
         static void Main(string[] args)
         {
+            ConfigToDictionary configToDictionary = new ConfigToDictionary();
+
             Console.WriteLine("Configuration to Dictionary - Function Kata");
 
             Console.WriteLine("Bitte geben Sie Ihr Konfigurationsfile ein");
 
             var input = Console.ReadLine();
 
-
+            Console.WriteLine(configToDictionary.ToString(input));
 
             Console.ReadKey();
         }
