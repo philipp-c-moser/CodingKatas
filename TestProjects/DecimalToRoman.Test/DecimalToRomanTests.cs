50d35ba3a339f90d6d608e48f973ba14cfc1e3d3
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-30 18:56:23 +0200

    DTR: SetUp TestClass

---
+++ TestProjects/DecimalToRoman.Test/DecimalToRomanTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace DecimalToRoman.Test
+{
+
+    [TestFixture]
+    class DecimalToRomanTests
+    {
+        private DecimalToRoman _decimalToRoman;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _decimalToRoman = new DecimalToRoman();
+        }
+
+    }
+}
