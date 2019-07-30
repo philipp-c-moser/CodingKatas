11b583845a193c66caa18aea3738c343cdae2e0c
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-07-30 20:03:43 +0200

    CTD: Add Method to split settings into key value pairs

--- FunctionKatas/ConfigToDictionary/ConfigToDictionary.cs
+++ FunctionKatas/ConfigToDictionary/ConfigToDictionary.cs
@@ -12,9 +12,16 @@ namespace ConfigToDictionary
         private IEnumerable<string> SplitSettings(string configuration)
         {
             var settings = configuration.Split(";", StringSplitOptions.RemoveEmptyEntries);
-
             
             return settings;
+        }
+
+        private IDictionary<string, string> SplitIntoKeyValuePairs(IEnumerable<string> settings)
+        {
+            var keyValuePairs = new Dictionary<string, string>();
+
+
+            return keyValuePairs;
         }
 
     }
