ede8d893957bcc1e132c5821af1eff545aaaa8bf
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-08-10 10:26:31 +0200

    FizzBuzz: Remove not required Text-Output

--- FunctionKatas/FizzBuzz/Program.cs
+++ FunctionKatas/FizzBuzz/Program.cs
@@ -7,16 +7,11 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             FizzBuzzHandler fizzBuzzHandler = new FizzBuzzHandler();
-
-            Console.WriteLine("FizzBuzz - FunctionKata");
-
-            Console.WriteLine("Please enter the amount of numbers, you want to generate:");
-
+            
             var amount = Convert.ToInt32(Console.ReadLine());
 
-
-
             var fizzBuzzString = fizzBuzzHandler.StartFizzBuzz(amount);
+
 
             foreach (var item in fizzBuzzString)
             {
